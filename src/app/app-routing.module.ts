82215509b8eb052c4b8f8ecb53699288d2dbf366
Pedro Ivo <7pedro.ivo@gmail.com>
2019-08-17 19:16:11 -0300

    Query Params implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
   { path: 'lista-carros', component: ListaCarrosComponent },
-  { path: 'lista-acessorios', component: AcessoriosCarroComponent, loadChildren: () => import('./home/lista-carros/lista-carros.module').then(m => m.ListaCarrosModule) }
+  { path: 'lista-acessorios/:carro', component: AcessoriosCarroComponent, loadChildren: () => import('./home/lista-carros/lista-carros.module').then(m => m.ListaCarrosModule) }
 
 
 ];
--- src/app/home/acessorios-carro/acessorios-carro.component.ts
+++ src/app/home/acessorios-carro/acessorios-carro.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-acessorios-carro',
   templateUrl: './acessorios-carro.component.html',
   styleUrls: ['./acessorios-carro.component.css']
 })
-export class AcessoriosCarroComponent {
+export class AcessoriosCarroComponent implements OnInit {
 
-  constructor() { }
+  public carro: any;
+
+  constructor(
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.carro = params.get('carro');
+      console.log(this.carro);
+
+    });
+  }
 
   public acessorios = [
-    { acessorio: 'Freio ABS - R$500.00' },
-    { acessorio: 'Ar Cond. - R$1.000.00' },
-    { acessorio: 'MP3 - R$800.00' }
+    { acessorio: 'Freio ABS', preco: 'R$500.00' },
+    { acessorio: 'Ar Cond.', preco: 'R$1.000.00' },
+    { acessorio: 'MP3', preco: 'R$800.00' }
   ];
 }
